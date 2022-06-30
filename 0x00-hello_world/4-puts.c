b695faf84e1b08580f191ff425db9248402d323f
koredeycode <koredey4u@gmail.com>
2022-06-30 19:39:27 +0000

    fourth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*This is the entry point with the function main*/
 int main(void)
+/*this is the entry point*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
