05c17a3a7d1275efbc89a76a5ebd3cccb938f2c0
koredeycode <koredey4u@gmail.com>
2022-07-01 23:12:26 +0000

    second to the last in advanced

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,24 +10,27 @@ int main(void)
 	int a;
 	int b;
 	int c;
+	int d;
 
 	a = 0;
 
 	while (a < 1000)
 	{
-		b = a % 100;
-		c = a / 100;
+		b = a / 100;
+		c = (a / 10) % 10;
+		d = a % 10;
 
-		if (c < b)
+		if (b < c && c < d)
 		{
-			putchar(c + '0');
 			putchar(b + '0');
-			if (a < 890)
+			putchar(c + '0');
+			putchar(d + '0');
+			if (a < 700)
 			{
 				putchar(',');
 				putchar(' ');
 			};
-		}
+		};
 		a++;
 	};
 	putchar('\n');
