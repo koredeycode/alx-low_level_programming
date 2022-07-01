36faa0529ca9e2c5e657afc5bd94537deb0d977f
koredeycode <koredey4u@gmail.com>
2022-07-01 17:15:34 +0000

    another one

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - contains the if statement
+ *
+ * Return:always return 0
+ *
+*/
+int main(void)
+{
+	int a = 97;
+
+	while (a < 123)
+	{
+	putchar(a);
+	a++;
+	};
+	putchar('\n');
+	return (0);
+}
