7f27a5a20e97f614eb31cc45fb71c1cf86849bfe
koredeycode <koredey4u@gmail.com>
2022-07-01 18:51:18 +0000

    8th task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - contains the if statement
+ *
+ * Return:always return 0
+ *
+*/
+int main(void)
+{
+	int a = 122;
+
+	while (a > 96)
+	{
+	putchar(a);
+	a--;
+	};
+	putchar('\n');
+	return (0);
+}
