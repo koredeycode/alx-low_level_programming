66d21c575c5d3a3d9deb62748975dc186beb849f
koredeycode <koredey4u@gmail.com>
2022-07-05 22:06:24 +0000

    second task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * main - Print "_putchar" followed by a new line.
- * Description: You are not allowed to include standared libraries.
+ * print_alphabet - print all the english alphabet
+ *
+ * Description - print alphabets in lowercase
+ *
  * Return: 0
  */
 void print_alphabet(void)
