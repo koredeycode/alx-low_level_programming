2c61cac597526dbc8f2f8e6faa14f9b1f8e89f4b
koredeycode <koredey4u@gmail.com>
2022-07-05 23:49:46 +0000

    push

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute value of the parameter
+ *
+ * @n: parameter of the int type
+ *
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n * 1);
+	}
+	else
+	{
+		return (n * -1);
+	}
+
+}
