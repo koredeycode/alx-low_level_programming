fc263ad095c563d20d996d166a4ab90b5eba89b1
koredeycode <koredey4u@gmail.com>
2022-07-05 21:57:01 +0000

    second task done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standared libraries.
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+		char first = 'a';
+		while (first <= 'z')
+		{
+			_putchar(first);
+			first++;
+		};
+		_putchar('\n');
+}
