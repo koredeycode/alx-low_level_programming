b4d9c5e71cb6f3cb239943405b753533d27c158b
koredeycode <koredey4u@gmail.com>
2022-07-05 19:21:33 +0000

    this

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,15 @@
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char word[] ={"_putchar"};
+	int i = 0;
+
+	while (i < 8)
+	{
+		
+		_putchar(word[i]);
+		i++;
+	};
 	_putchar('\n');
 
 	return (0);
