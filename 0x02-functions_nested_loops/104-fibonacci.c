1402072673011b6eeead0f19bb3850f213d4aad8
koredeycode <koredey4u@gmail.com>
2022-07-06 20:07:58 +0000

    this

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - print first 98 fibonnaci numbers
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int first = 1;
+	long int second = 2;
+	int count = 1;
+	long int sum;
+	
+	printf("%lu, %lu,", first, second);
+
+	while (count <= 96)
+	{
+		sum = first + second;
+
+		if (count != 96)
+		{
+			printf(" %lu,", sum);
+		}
+		else
+		{
+			printf(" %lu", sum);
+		}
+	first = second;
+	second = sum;
+	count++;
+	}
+	putchar('\n');
+	return (0);
+}
