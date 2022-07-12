ee95db6af55be3657683cbab99ff2c9202654a9b
koredeycode <koredey4u@gmail.com>
2022-07-12 23:00:49 +0000

    this

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,32 +8,4 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	if (i % 2 == 0)
-	{
-		int j = i / 2;
-
-		while (j <= i)
-		{
-			printf("%c", str[j]);
-			j++;
-		}
-		printf("\n");
-	}
-	else
-	{
-		int k = (i - 1) / 2;
-
-		while (k <= i)
-		{
-			printf("%c", str[k]);
-			k++;
-		}
-		printf("\n");
-	}
 }
