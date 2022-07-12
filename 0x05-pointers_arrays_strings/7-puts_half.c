f341558029a18d6b73ab98e8ea5952b884f18c64
koredeycode <koredey4u@gmail.com>
2022-07-12 22:49:54 +0000

    this

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * puts_half - reset the values of the inputs
+ * @str: pointer, a character
+ * Return: the length of the string
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		int j = i / 2;
+
+		while (j <= i)
+		{
+			printf("%c", str[j]);
+			j++;
+		}
+		printf("\n");
+	}
+	else
+	{
+		int k = (i - 1) / 2;
+
+		while (k <= i)
+		{
+			printf("%c", str[k]);
+			k++;
+		}
+		printf("\n");
+	}
+}
