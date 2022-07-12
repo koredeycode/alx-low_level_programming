c85d5de4cb2c4752ec1f070be37e551b98a5f9c6
koredeycode <koredey4u@gmail.com>
2022-07-12 21:17:53 +0000

    this

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - reset the value of input to 98.
+ * @n: pointer, an integer
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
+}
