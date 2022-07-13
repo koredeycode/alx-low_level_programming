45366c50375f8a6acfe26f49b782ca0295eb9fed
koredeycode <koredey4u@gmail.com>
2022-07-13 11:24:28 +0000

    this

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,14 +8,17 @@
 
 void print_array(int *a, int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		printf("%d", a[i]);
-		if (i < (n - 1))
+		int i;
+
+		for (i = 0; i < n; i++)
 		{
-			printf(", ");
+			printf("%d", a[i]);
+			if (i < (n - 1))
+			{
+				printf(", ");
+			}
 		}
 	}
 }
