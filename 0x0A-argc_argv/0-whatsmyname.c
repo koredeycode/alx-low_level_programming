96ce4290820d62b623583cdd3134ada80f57b8b2
koredeycode <koredey4u@gmail.com>
2022-07-22 17:23:44 +0000

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print a program name followed by a new line
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: always 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+this is a readme file for this directory containing project tasks for alx argc and argv for alx 
+
+
