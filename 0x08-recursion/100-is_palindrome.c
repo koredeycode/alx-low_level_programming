1ce17042adab77c45fd0eaeb7bc641a778a4c338
koredeycode <koredey4u@gmail.com>
2022-07-22 17:31:03 +0000

    this

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+int palhelper(char *s, char *t)
+{
+}
+/**
+ * is_palindrome - returns if a number is palindrome or not
+ * @s: a character pointer
+ *
+ * Return: an integer
+ */
+int is_palindrome(char *s)
+{
+	if
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-This repo contains the project tasks for alx low level programming
