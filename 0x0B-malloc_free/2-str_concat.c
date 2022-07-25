5853475b0c972fe9e2b38513f812585266cb0ba7
koredeycode <koredey4u@gmail.com>
2022-07-25 18:08:05 +0000

    this

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,8 +11,8 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int i;
 	unsigned int j;
-	unsigned int len1 = strlen(s1);
-	unsigned int len2 = strlen(s2);
+	unsigned int len1;
+	unsigned int len2;
 	char *sout;
 
 	if (s1 == NULL)
@@ -23,6 +23,8 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
+	len1 = strlen(s1);
+	len2 = strlen(s2)
 	sout = malloc((len1 + len2 + 1) * sizeof(char));
 	if (sout == NULL)
 	{
