bf59000a4849a89020e060101b4f09d8fd3fa8d3
koredeycode <koredey4u@gmail.com>
2022-07-25 16:03:23 +0000

    this

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	if (size ==  0)
+	{
+		return (NULL);
+	}
+	arr = malloc(size * sizeof(char));
+	
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is the readme file about malloc and free for the alx Software Engineering programme tasks
