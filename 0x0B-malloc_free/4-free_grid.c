e1a20cb9247f8d3e75399ef01b4c399eea9918ff
koredeycode <koredey4u@gmail.com>
2022-07-26 14:34:31 +0000

    this

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,12 +3,11 @@
 
 /**
  * free_grid - free a memory assigned to a 2 dimensional array of integers.
+ * @grid - the inputted multi dimensional array
  * @height: height of the array.
- *
- * Return: pointer of an array of integers
  */
 void free_grid(int **grid, int height)
-{	
+{
 	if (grid != NULL && height != 0)
 	{
 		for (; height >= 0; height--)
@@ -16,5 +15,5 @@ void free_grid(int **grid, int height)
 			free(grid[height]);
 		}
 	}
-	free(grid);	
+	free(grid);
 }
