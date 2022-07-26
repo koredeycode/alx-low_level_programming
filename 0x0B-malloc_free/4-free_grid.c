37e1f1c37937ef13c19c3ea989cd7fd1b1d8225f
koredeycode <koredey4u@gmail.com>
2022-07-26 14:31:45 +0000

    this

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,6 +8,13 @@
  * Return: pointer of an array of integers
  */
 void free_grid(int **grid, int height)
-{
+{	
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+		{
+			free(grid[height]);
+		}
+	}
 	free(grid);	
 }
