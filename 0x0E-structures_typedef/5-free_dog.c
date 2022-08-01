1193104aae8288ea9faf60a9ee53cddcafd6036a
koredeycode <koredey4u@gmail.com>
2022-08-01 17:56:25 +0000

    this

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,15 @@
+#include <stdlib.h>
 #include "dog.h"
 /**
  * free_dog - free the struct off heap memory
  * @d: the inputted struct
  */
-void free_dog(dog_t *d);
+void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		if (!(d->name) || !(d->age) || !(d->owner))
-		{
-			free(d->name);
-			free(d->age);
-			free(d->owner);
-			free(d);
-		}
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
 }
