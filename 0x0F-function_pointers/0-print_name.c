deb6ff01cef8e2643407e5f4bf130a0346fb5e16
koredeycode <koredey4u@gmail.com>
2022-08-03 17:41:25 +0000

    this

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,8 @@
+/**
+ * print_name - print a name according to a function input
+ * @name: name to be inputted
+ * @f: pointer to a function
+ */
 void print_name(char *name, void(*f)(char *))
 {
 	f(name);
