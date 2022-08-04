b2a01101b50c30600bc33e65ff0f86835732663b
koredeycode <koredey4u@gmail.com>
2022-08-04 18:28:56 +0000

    this

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,31 +13,50 @@ void print_all(const char * const format, ...)
 	char *str;
 	int n;
 	char c;
+	int j;
+	int d = 0;
 	float f;
+	char *specs = "cifs";
 
 	va_start(params, format);
-	while (format[i] != '\0')
+	while (format && format[i])
 	{
+		j = 0;
+		while (specs[j])
+		{
+			if (format[i] == specs[j] && d)
+			{
+				printf(", ");
+				break;
+			}
+			j++;
+		}
 		switch (format[i])
 		{
 			case 'c':
 				c  = va_arg(params, int);
-				printf("%c, ", c);
+				printf("%c", c);
+				d = 1;
 				break;
 			case 'i':
 				n = va_arg(params, int); 
-				printf("%i, ", n);
+				printf("%i", n);
+				d = 1;
 				break;
 			case 'f':
 				f = va_arg(params, double);
-				printf("%f, ", f);
+				printf("%f", f);
+				d = 1;
 				break;
 			case 's':
 				str = va_arg(params, char *);
+				d = 1;
 				if (str == NULL)
+				{
 					printf("(nil)");
-				if (str)
-					printf("%s, ", str);
+					break;
+				}
+					printf("%s", str);
 				break;
 		}
 		i++;
