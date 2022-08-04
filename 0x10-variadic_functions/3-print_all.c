da6620d6f2d1e983e9d0db57ac73dfc89b75e300
koredeycode <koredey4u@gmail.com>
2022-08-04 18:40:43 +0000

    this

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,55 +2,41 @@
 #include <stdarg.h>
 #include <stdlib.h>
 /**
- * print_strings - prints strings followed by a line
- * @separator: string to be printed between the strings
- * @n: number of strings passed to the function
+ * print_all - prints  parameters based on specifiers
+ * @format: contain the format specifiers
  */
 void print_all(const char * const format, ...)
 {
 	unsigned int i = 0;
 	va_list params;
-	char *str;
-	int n;
-	char c;
-	int j;
-	int d = 0;
+	char c, *str, *specs = "cifs";
+	int n, j, d = 0;
 	float f;
-	char *specs = "cifs";
 
 	va_start(params, format);
 	while (format && format[i])
-	{
-		j = 0;
+	{ j = 0;
 		while (specs[j])
 		{
 			if (format[i] == specs[j] && d)
 			{
 				printf(", ");
 				break;
-			}
-			j++;
+			} j++;
 		}
 		switch (format[i])
 		{
 			case 'c':
-				c  = va_arg(params, int);
-				printf("%c", c);
-				d = 1;
+				c  = va_arg(params, int), printf("%c", c), d = 1;
 				break;
 			case 'i':
-				n = va_arg(params, int); 
-				printf("%i", n);
-				d = 1;
+				n = va_arg(params, int), printf("%i", n), d = 1;
 				break;
 			case 'f':
-				f = va_arg(params, double);
-				printf("%f", f);
-				d = 1;
+				f = va_arg(params, double), printf("%f", f), d = 1;
 				break;
 			case 's':
-				str = va_arg(params, char *);
-				d = 1;
+				str = va_arg(params, char *), d = 1;
 				if (str == NULL)
 				{
 					printf("(nil)");
@@ -58,9 +44,7 @@ void print_all(const char * const format, ...)
 				}
 					printf("%s", str);
 				break;
-		}
-		i++;
+		} i++;
 	}
-	va_end(params);
-	printf("\n");
+	va_end(params),	printf("\n");
 }
