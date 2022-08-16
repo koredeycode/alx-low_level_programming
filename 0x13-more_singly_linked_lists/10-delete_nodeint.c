38cee1f0bb307cbc66bff60e377dee55dfc6a81a
koredeycode <koredey4u@gmail.com>
2022-08-16 16:32:07 +0000

    this

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	prv = *head;
 	if (index != 0)
 	{
-		for (i = 0; i < index -1 && prv != NULL; i++)
+		for (i = 0; i < index - 1 && prv != NULL; i++)
 		{
 			prv = prv->next;
 		}
