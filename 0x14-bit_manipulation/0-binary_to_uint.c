7db9bed63f27a916cb927572c5e23ef797402d90
koredeycode <koredey4u@gmail.com>
2022-08-18 08:59:13 +0000

    first_task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * _power - return the result of base to power exp
+ * @base: unsigned int
+ * @exp: unsigned int
+ * Return: the result
+ */
+unsigned int _power(unsigned int base, unsigned int exp)
+{
+	unsigned int res = 1;
+
+	while (exp--)
+	{
+		res *= base;
+	}
+	return (res);
+}
+/**
+ * check - check if the string contain another character
+ * @b: string
+ * Return: 0 or 1
+ */
+unsigned int check(const char *b)
+{
+	unsigned int i = 0;
+
+	while (b[i] != '\0')
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: a string of 0 and 1 chars
+ * Return: the converted number or 0 if b is NULL or b contain chars not 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0, ret, sum = 0;
+
+	if (b == NULL || check(b) == 0)
+	{
+		return (0);
+	}
+	ret = atoi(b);
+	while (ret != 0)
+	{
+		sum += (ret % 10) * _power(2, i);
+		i++;
+		ret /= 10;
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This folder contain the project tasks on bit manipulation for the software engineering programme at alx
