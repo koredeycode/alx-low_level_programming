b83664371d31962bb69e893d3ee3906d8f00524e
koredeycode <koredey4u@gmail.com>
2022-08-22 14:23:47 +0000

    this

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,12 +8,18 @@
 int create_file(const char *filename, char *text_content)
 {
 	int f;
-	/*ssize_t k;*/
+	int k;
 
 	if (filename == NULL)
 		return (-1);
 	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
-	write(f, text_content, strlen(text_content));
+	if (f == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	k = write(f, text_content, strlen(text_content));
+	if (k == -1)
+		return (-1);
 	close(f);
 	return (1);
 }
