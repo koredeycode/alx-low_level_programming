33d5c6fe51bfbe7eb8a3df566f3822ce56036ee1
koredeycode <koredey4u@gmail.com>
2022-08-22 13:51:38 +0000

    this

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  * Return: the actual number of letters it could read and print
  */
 
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t nrd, nwr;
