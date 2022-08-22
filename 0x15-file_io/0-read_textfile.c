57ae16506ea40cd3317480c5f55233ab2782a5af
koredeycode <koredey4u@gmail.com>
2022-08-22 13:47:18 +0000

    this

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and print it to the posix stdout
+ * @filename: the file to be read
+ * @letters: the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buff;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	nrd = read(fd, buff, letters);
+	nwr = write(STDOUT_FILENO, buff, nrd);
+	close(fd);
+	free(buff);
+	return (nwr);
+}
