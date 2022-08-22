ca52a152c684eab9aa7db946c2df22c6c77a07ab
koredeycode <koredey4u@gmail.com>
2022-08-22 14:15:53 +0000

    this

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * create_file - create a file
+ * @filename: the name of the file to be created
+ * @text_content: the content of the file
+ * Return: an integer
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int f;
