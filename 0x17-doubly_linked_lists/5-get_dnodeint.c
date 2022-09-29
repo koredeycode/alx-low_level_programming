f44b04ca5f32f70e697c2d6d37d8913dbb8f2b27
koredeycode <koredey4u@gmail.com>
2022-09-29 21:45:37 +0000

    this

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get the value at a node from 0
+ * @head: head of the node
+ * @index: index
+ * Return: the value at the index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *h = head;
+	unsigned int i;
+
+	if (!head)
+	{
+		return (NULL);
+	}
+	if (head)
+		while (head->prev != NULL)
+			head = head->prev;
+	for (i = 0; i < index; i++)
+	{
+		h = h->next;
+	}
+	return (h);
+}
