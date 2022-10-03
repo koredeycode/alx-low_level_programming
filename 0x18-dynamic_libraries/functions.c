4ec573074aace876eec606cfe5bbfb2f787eaa45
koredeycode <koredey4u@gmail.com>
2022-10-03 12:58:49 +0000

    this

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,50 @@
+/**
+ * add - add two integers
+ * @a: int a
+ * @b: int b
+ * Return: return a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtract two integers
+ * @a: int a
+ * @b: int b
+ * Return: return a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiply two integers
+ * @a: int a
+ * @b: int b
+ * Return: return a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - add two integers
+ * @a: int a
+ * @b: int b
+ * Return: return a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - find the mod between two integers
+ * @a: int a
+ * @b: int b
+ * Return: return a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
