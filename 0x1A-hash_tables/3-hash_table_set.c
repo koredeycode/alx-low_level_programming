8e7b3c940942c6eabd1e8a5ec0faa0040e6037e5
koredeycode <koredey4u@gmail.com>
2022-10-20 21:49:36 +0000

    this

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,48 +9,37 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int size = ht->size;
-	unsigned long int idx = key_index((const unsigned char *)key, size);
+	unsigned long int idx;
 	hash_node_t *ins/*= malloc(sizeof(hash_node_t))*/;
 	hash_node_t *tmp;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+
+	if (tmp)
+	{
+		while (tmp)
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				tmp->value = strdup(value);
+				return (1);
+			}
+			tmp = tmp->next;
+		}
+	}
+
 	ins = malloc(sizeof(hash_node_t));
 	if (ins == NULL)
 		return (0);
-	ins->key = (char *)key;
-	ins->value = (char *)value;
+	ins->key = strdup(key);
+	ins->value = strdup(value);
 	ins->next = NULL;
-	if (ht->array[idx] == NULL)
-	{
-		ht->array[idx] = ins;
-		return (1);
-	}
-	else/*slot not empty*/
-	{
-		if (strcmp(ht->array[idx]->key, key) == 0)/*slot key=key,change the val*/
-		{
-			ht->array[idx]->value = (char *)value;
-			return (1);
-		}
-		else/*slot key is not key but same idx, collision, add to the linked list*/
-		{
-			tmp = ht->array[idx];
-			while (tmp)
-			{
-				if (strcmp(tmp->key, key) == 0)
-				{
-					tmp->value = (char *)value;
-					return (1);
-				}
-				tmp = tmp->next;
-			}
-			tmp = ht->array[idx];
-			ins->next = tmp;
-			ht->array[idx] = ins;
-			return (1);
-		}
-	}
-	return (0);
+	if (ht->array[idx] != NULL)
+		ins->next = ht->array[idx];
+	ht->array[idx] = ins;
+	return (1);
 }
