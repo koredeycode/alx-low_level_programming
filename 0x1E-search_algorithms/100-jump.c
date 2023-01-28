68c6269152a280abedc9576fc47cf1f4d9d68c78
koredeycode <koredey4u@gmail.com>
2023-01-28 00:50:03 +0000

    this

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,7 +14,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t b = (size_t)sqrt(size);
 	size_t sqt = 0;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
 	while (a < size && array[a] < value)
 	{
